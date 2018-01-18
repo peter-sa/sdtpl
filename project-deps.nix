44bbfdbe069509380bb478e25544b5d1e535c1f4
Keith Amidon <keith@awakenetworks.com>
2018-01-18 10:26:49 -0800

    Pin nixpkgs to tip of the 17.09 branch for release builds
    At the time of commit this was the most recent stable NixOS release
    commit. The current plan is to move to the most recent NixOS stable
    release for each release of this project.

--- project-deps.nix
+++ project-deps.nix
@@ -4,8 +4,8 @@ self: super:
   nixpkgs = self.releaseGHDevLocal {
     owner = "NixOS";
     repo = "nixpkgs";
-    rev = "TODO";
-    sha256 = "TODO";
+    rev = "66b4de79e3841530e6d9c6baf98702aa1f7124e4";
+    sha256 = "1l3lwi944hnxka0nfq9a1g86xhc0b8hzqr2fm6cvds33gj26l0g4";
     local = <nixpkgs>;
     fetch = super.nixpkgs.fetchFromGitHub;
   } { config = {}; };
